97d860d6de44ca6380223fe5ce562a5f646e45b4
Adam <Adam@Adams-MacBook-Pro.local>
2013-09-13 21:31:28 -0700

    Defining maze size

--- MazeSolver/main.cpp
+++ MazeSolver/main.cpp
@@ -8,13 +8,28 @@
 
 #include <iostream>
 
+#define MAX_SIZE_X 30
+#define MAX_SIZE_Y 30
+
 using namespace std;
 
 int main(int argc, const char * argv[])
 {
+  int xinput;
+  int yinput;
+  
+  do{
+  cout<<"How tall would you like your maze to be?";
+  cin>>xinput;
+  }while(xinput>MAX_SIZE_X);
+  
+  do{
+    cout<<"How wide would you like your maze to be?";
+    cin>>yinput;
+  }while(yinput>MAX_SIZE_X);
+  
 
-    // insert code here...
-    cout << "Hello, World!\n";
-    return 0;
+  
+  
 }
 

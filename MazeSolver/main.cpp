2b955cd0b9cc87d44e0d0d6a63e1ed6988368671
Adam <Adam@Adams-MacBook-Pro.local>
2013-09-13 20:54:33 -0700

    Initial commit
    added: using namespace std;

--- MazeSolver/main.cpp
+++ MazeSolver/main.cpp
@@ -8,11 +8,13 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, const char * argv[])
 {
 
     // insert code here...
-    std::cout << "Hello, World!\n";
+    cout << "Hello, World!\n";
     return 0;
 }
 

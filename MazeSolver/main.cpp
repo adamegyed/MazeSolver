17324494434816304c7a7c14f75913fe267559e6
Adam <Adam@Adams-MacBook-Pro.local>
2013-09-13 20:47:04 -0700

    Initial Commit

---
+++ MazeSolver/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MazeSolver
+//
+//  Created by Adam Egyed on 9/13/13.
+//  Copyright (c) 2013 Adam Egyed. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
